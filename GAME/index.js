8c0082d8f238a310612b3b97e5e285ceb79adbd0
Roseno  Silva <70597064+sZeuSz@users.noreply.github.com>
2020-12-14 23:43:14 -0400

    Add files via upload

---
+++ GAME/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const handlebars =  require("express-handlebars");
+const router = require("./config/router");
+const logger = require("morgan");
+const sass = require("node-sass-middleware");
+
+const app = express();
+
+/*
+app.engine("handlebars", handlebars({
+	helpers: require(`${__dirname}/views/helpers/index`)
+}));
+*/
+
+app.engine("handlebars", handlebars());
+app.set("view engine","handlebars");
+app.set("views", `${__dirname}/app/views`);
+
+app.use(sass({
+	src: `${__dirname}/public/scss`,
+	dest: `${__dirname}/public/css`,
+	outputStyle: 'compressed',
+	prefix: '/css'
+
+}));
+
+app.use(logger("combined"));
+
+app.use(express.urlencoded({extended: false}));
+
+app.use(router);
+
+app.use("/img", express.static(`${__dirname}/public/img`) );
+app.use("/css", express.static(`${__dirname}/public/css`) );
+app.use("/webfonts", express.static(`${__dirname}/node_modules/@fortawesome/fontawesome-free/webfonts`));
+
+app.use("/js", [
+	express.static(__dirname + '/node_modules/jquery/dist'),
+	express.static(__dirname + '/node_modules/popper.js/dist/umd'),
+	express.static(__dirname + '/node_modules/bootstrap/dist/js'),
+	express.static(__dirname + '/public/js')
+]);
+
+
+app.listen(3000, function() {
+	console.log("Express app iniciada na porta 3000.");
+});
+
